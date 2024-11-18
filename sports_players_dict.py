a1e42e8d4bb2d902207c1be5c8c1156266e18445
jrmh1 <151538560+jrmh1@users.noreply.github.com>
2024-11-18 10:42:36 +0800

    Update sports_players_dict.py

--- sports_players_dict.py
+++ sports_players_dict.py
@@ -1,1 +1,29 @@
+sports_players_dict = {
+    'Soccer': 'Lionel Messi',
+    'Basketball': 'LeBron James',
+    'Tennis': 'Roger Federer',
+    'Baseball': 'Babe Ruth',
+    'Cricket': 'Sachin Tendulkar',
+    'Boxing': 'Muhammad Ali',
+    'Golf': 'Tiger Woods',
+    'Rugby': 'Jonah Lomu',
+    'Swimming': 'Michael Phelps',
+    'American Football': 'Tom Brady'
+}
+
+
+print("Entire dictionary:", sports_players_dict)
+
+
+fourth_sport = list(sports_players_dict.items())[3]
+print("Player of the 4th sport:", fourth_sport)
+
+
+sports_players_dict['Boxing'] = 'Mike Tyson'
+
+del sports_players_dict['American Football']
+
+
+last_sport = list(sports_players_dict.items())[-1]
+print("Last key-value pair:", last_sport)
 

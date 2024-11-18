fa62cb73ca8aa4c0c420a0c2c0ef3b804acbd895
jrmh1 <151538560+jrmh1@users.noreply.github.com>
2024-11-19 01:31:28 +0800

    Update dog_breeds_dict.py

--- dog_breeds_dict.py
+++ dog_breeds_dict.py
@@ -1,1 +1,24 @@
+dog_breeds_dict = {
+    'Beagle': 'Medium',
+    'Chihuahua': 'Small',
+    'Golden Retriever': 'Large',
+    'German Shepherd': 'Large',
+    'Poodle': 'Medium',
+    'Bulldog': 'Medium',
+    'Rottweiler': 'Large',
+    'Dachshund': 'Small',
+    'Boxer': 'Medium',
+    'Labrador Retriever': 'Large'
+}
 
+print("Entire dictionary:", dog_breeds_dict)
+
+fifth_breed_size = list(dog_breeds_dict.items())[4]
+print("Size of the 5th breed:", fifth_breed_size)
+
+dog_breeds_dict['Dachshund'] = 'Medium'
+
+del dog_breeds_dict['Bulldog']
+
+last_breed_size = list(dog_breeds_dict.items())[-1]
+print("Last key-value pair:", last_breed_size)
